07e708c6ca20f8df042d9f85e5f96036a9901007
Tinko-cpu <oseninurudeenafolabi@yahoo.com>
2022-04-20 16:14:58 +0100

    update

--- benchmark.js
+++ benchmark.js
@@ -1,123 +1,5 @@
-'use strict';
+// QUESTIONS
 
- // Credit: Mateusz Rybczonec
-
- const FULL_DASH_ARRAY = 283;
- const WARNING_THRESHOLD = 10;
- const ALERT_THRESHOLD = 5;
-
- const COLOR_CODES = {
-   info: {
-     color: "green",
-   },
-   warning: {
-     color: "orange",
-     threshold: WARNING_THRESHOLD,
-   },
-   alert: {
-     color: "red",
-     threshold: ALERT_THRESHOLD,
-   },
- };
-
- const TIME_LIMIT = 10;
- let timePassed = 0;
- let timeLeft = TIME_LIMIT;
- let timerInterval = null;
- let remainingPathColor = COLOR_CODES.info.color;
-
- document.getElementById("app").innerHTML = `
-<div class="base-timer">
-<svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-<g class="base-timer__circle">
- <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
- <path
-   id="base-timer-path-remaining"
-   stroke-dasharray="283"
-   class="base-timer__path-remaining ${remainingPathColor}"
-   d="
-     M 50, 50
-     m -45, 0
-     a 45,45 0 1,0 90,0
-     a 45,45 0 1,0 -90,0
-   "
- ></path>
-</g>
-</svg>
-<span id="base-timer-label" class="base-timer__label">${formatTime(
-timeLeft
-)}</span>
-</div>
-`;
-
- startTimer();
-
- function onTimesUp() {
-   clearInterval(timerInterval);
- }
-
- function startTimer() {
-   timerInterval = setInterval(() => {
-     timePassed = timePassed += 1;
-     timeLeft = TIME_LIMIT - timePassed;
-     document.getElementById("base-timer-label").innerHTML =
-       formatTime(timeLeft);
-     setCircleDasharray();
-     setRemainingPathColor(timeLeft);
-
-     if (timeLeft === 0) {
-       onTimesUp();
-     }
-   }, 1000);
- }
-
- function formatTime(time) {
-   // const minutes = Math.floor(time / 60);
-   // let seconds = time % 60;
-   let seconds = time;
-
-   if (seconds < 10) {
-     seconds = `0${seconds}`;
-   }
-
-   // return `${minutes}:${seconds}`;
-   return `${seconds}`;
- }
-
- function setRemainingPathColor(timeLeft) {
-   const { alert, warning, info } = COLOR_CODES;
-   if (timeLeft <= alert.threshold) {
-     document
-       .getElementById("base-timer-path-remaining")
-       .classList.remove(warning.color);
-     document
-       .getElementById("base-timer-path-remaining")
-       .classList.add(alert.color);
-   } else if (timeLeft <= warning.threshold) {
-     document
-       .getElementById("base-timer-path-remaining")
-       .classList.remove(info.color);
-     document
-       .getElementById("base-timer-path-remaining")
-       .classList.add(warning.color);
-   }
- }
-
- function calculateTimeFraction() {
-   const rawTimeFraction = timeLeft / TIME_LIMIT;
-   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
- }
-
- function setCircleDasharray() {
-   const circleDasharray = `${(
-     calculateTimeFraction() * FULL_DASH_ARRAY
-   ).toFixed(0)} 283`;
-   document
-     .getElementById("base-timer-path-remaining")
-     .setAttribute("stroke-dasharray", circleDasharray);
- }
-
-// STARTING OF THE QUESTIONS ARRAY
 const questions = [
     {
       category: "Science: Computers",
@@ -217,33 +99,180 @@ const questions = [
       incorrect_answers: ["Python", "C", "Jakarta"],
     },
   ];
-// SELECTING THE ELEMENTS
+
+ // Credit: Mateusz Rybczonec
+
+ const FULL_DASH_ARRAY = 283;
+ const WARNING_THRESHOLD = 10;
+ const ALERT_THRESHOLD = 5;
+
+ const COLOR_CODES = {
+   info: {
+     color: "green",
+   },
+   warning: {
+     color: "orange",
+     threshold: WARNING_THRESHOLD,
+   },
+   alert: {
+     color: "red",
+     threshold: ALERT_THRESHOLD,
+   },
+ };
+
+
+
+ const TIME_LIMIT = 10;
+ let timePassed = 0;
+ let timeLeft = TIME_LIMIT;
+ let timerInterval = null;
+ let remainingPathColor = COLOR_CODES.info.color;
+
+ document.getElementById("app").innerHTML = `
+<div class="base-timer">
+<svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+<g class="base-timer__circle">
+ <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+ <path
+   id="base-timer-path-remaining"
+   stroke-dasharray="283"
+   class="base-timer__path-remaining ${remainingPathColor}"
+   d="
+     M 50, 50
+     m -45, 0
+     a 45,45 0 1,0 90,0
+     a 45,45 0 1,0 -90,0
+   "
+ ></path>
+</g>
+</svg>
+<span id="base-timer-label" class="base-timer__label">${formatTime(
+timeLeft
+)}</span>
+</div>
+`;
+
 let h2 = document.querySelector("h2");
-let randNumber = Math.trunc(Math.random() * questions.length);
+
+let btns = document.querySelectorAll(".btn");
 
 // TO GET THE RANDOM OBJECT INSIDE ARRAYS
 function ranQuestionsObj(){
+    let randNumber = Math.trunc(Math.random() * questions.length);
     const randObject = questions[randNumber];
     return randObject;
 }
 
 
 
-console.log(ranQuestionsObj());
+// console.log(ranQuestionsObj());
 // // INITIALISE MY WORK
-let question = ranQuestionsObj().question;
+
 let incorrectAnswers = ranQuestionsObj().incorrect_answers;
 let correctAnswer = ranQuestionsObj().correct_answer;
 
-console.log(`this is my question ${question}`);
-console.log(`Correct answer ${correctAnswer}`);
-console.log(`incorrect answer ${incorrectAnswers}`);
 
-let myTime = setInterval(function(){
-    
-}, 1000)
+let myTime = new Date().getSeconds();
+let mySec = Number(myTime.toString().slice(0,2));
 
-if(myTime === 2){
-    question = ranQuestionsObj().question;
+function randomQuestion(){
+    let question = ranQuestionsObj().question;
+    console.log("Hello Lidia");
     h2.innerText = question;
+    for(let i = 0; i < btns.length; i++){
+        let answer = [...incorrectAnswers, correctAnswer];
+        btns[i].innerText = answer[i];
+        if(answer[i] === undefined){
+            btns[i].classList.add("hidden");
+        }
+    }
 }
+
+randomQuestion();
+ startTimer();
+
+ function onTimesUp() {
+   clearInterval(timerInterval);
+ }
+
+ function startTimer() {
+    setInterval(() => {
+     timePassed = timePassed += 1;
+     timeLeft = TIME_LIMIT - timePassed;
+     document.getElementById("base-timer-label").innerHTML =
+       formatTime(timeLeft);
+     setCircleDasharray();
+     setRemainingPathColor(timeLeft);
+
+     if (timeLeft === 0) {
+        console.log("Times Up");
+    //    onTimesUp();
+        timeLeft = 10;
+       randomQuestion();
+
+     }
+   }, 1000);
+ }
+
+ function formatTime(time) {
+   // const minutes = Math.floor(time / 60);
+   // let seconds = time % 60;
+   let seconds = time;
+
+   if (seconds < 10) {
+     seconds = `0${seconds}`;
+   }
+
+   // return `${minutes}:${seconds}`;
+   return `${seconds}`;
+ }
+
+ function setRemainingPathColor(timeLeft) {
+   const { alert, warning, info } = COLOR_CODES;
+   if (timeLeft <= alert.threshold) {
+     document
+       .getElementById("base-timer-path-remaining")
+       .classList.remove(warning.color);
+     document
+       .getElementById("base-timer-path-remaining")
+       .classList.add(alert.color);
+   } else if (timeLeft <= warning.threshold) {
+     document
+       .getElementById("base-timer-path-remaining")
+       .classList.remove(info.color);
+     document
+       .getElementById("base-timer-path-remaining")
+       .classList.add(warning.color);
+   }
+ }
+
+ function calculateTimeFraction() {
+   const rawTimeFraction = timeLeft / TIME_LIMIT;
+   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+ }
+
+ function setCircleDasharray() {
+   const circleDasharray = `${(
+     calculateTimeFraction() * FULL_DASH_ARRAY
+   ).toFixed(0)} 283`;
+   document
+     .getElementById("base-timer-path-remaining")
+     .setAttribute("stroke-dasharray", circleDasharray);
+ }
+
+// STARTING OF THE QUESTIONS ARRAY
+
+// SELECTING THE ELEMENTS
+
+
+
+// console.log(`this is my question ${question}`);
+// console.log(`Correct answer ${correctAnswer}`);
+// console.log(`incorrect answer ${incorrectAnswers}`);
+
+
+
+
+
+
+
