0461531b45fb679b9adbf7bfdb5dc6965ee36470
SidathDabare <sidath2007@gmail.com>
2022-04-22 01:04:15 +0200

    resultpage functionality and some css fixing

--- javascript-file/benchmark.js
+++ javascript-file/benchmark.js
@@ -1,6 +1,5 @@
 // QUESTIONS
 
-
 const questions = [
   {
     category: "Science: Computers",
@@ -68,11 +67,7 @@ const questions = [
     question:
       "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
-    incorrect_answers: [
-      "Ice Cream Sandwich",
-      "Jelly Bean",
-      "Marshmallow",
-    ],
+    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
   },
   {
     category: "Science: Computers",
@@ -101,121 +96,103 @@ const questions = [
   },
 ];
 
-let questionCount = 1
-let correctAnswersCount = 0
+let questionCount = 1;
+let correctAnswersCount = 0;
 
+questions.forEach(function (Question) {
+  let questionsAndAnswersDivNode = document.createElement("div");
+  let questionAndAnswersHolderNode = document.getElementById(
+    "questions-container"
+  );
+  questionAndAnswersHolderNode.appendChild(questionsAndAnswersDivNode);
 
-  questions.forEach(function(Question){
-    let questionsAndAnswersDivNode = document.createElement('div')
-    let questionAndAnswersHolderNode = document.getElementById('questions-container')
-    questionAndAnswersHolderNode.appendChild(questionsAndAnswersDivNode)
+  let questionH2Node = document.createElement("h2");
+  questionH2Node.innerText = Question.question;
+  questionH2Node.classList.add("question-style");
 
-    let questionH2Node = document.createElement('h2')
-    questionH2Node.innerText = Question.question
-    questionH2Node.classList.add('question-style')
-    
-    questionsAndAnswersDivNode.appendChild(questionH2Node)
-    let correctAnsButtonNode = document.createElement('button')
-    correctAnsButtonNode.innerText = Question.correct_answer
-    correctAnsButtonNode.classList.add('btn')
-    correctAnsButtonNode.setAttribute('id','correct')
-    questionsAndAnswersDivNode.appendChild(correctAnsButtonNode)
-    Question.incorrect_answers.forEach(function(answer){
-      let incorrectAnsButtonNode = document.createElement('button')
-      incorrectAnsButtonNode.innerText = answer
-      incorrectAnsButtonNode.classList.add('btn')
-      questionsAndAnswersDivNode.appendChild(incorrectAnsButtonNode)
-    })
-})
-let allDivsContainingQuestionsNode = document.querySelectorAll('#questions-container > div')
-allDivsContainingQuestionsNode.forEach(function(element){
-  element.classList.add('display-not')
-
-})
-allDivsContainingQuestionsNode[0].classList.remove('display-not')
-let allButtonsWithAnswersNodes =document.getElementsByClassName('btn')
-for(let node of allButtonsWithAnswersNodes){
-  node.addEventListener('click',function(){ node.classList.toggle('selected')})
+  questionsAndAnswersDivNode.appendChild(questionH2Node);
+  let correctAnsButtonNode = document.createElement("button");
+  correctAnsButtonNode.innerText = Question.correct_answer;
+  correctAnsButtonNode.classList.add("btn");
+  correctAnsButtonNode.setAttribute("id", "correct");
+  questionsAndAnswersDivNode.appendChild(correctAnsButtonNode);
+  Question.incorrect_answers.forEach(function (answer) {
+    let incorrectAnsButtonNode = document.createElement("button");
+    incorrectAnsButtonNode.innerText = answer;
+    incorrectAnsButtonNode.classList.add("btn");
+    questionsAndAnswersDivNode.appendChild(incorrectAnsButtonNode);
+  });
+});
+let allDivsContainingQuestionsNode = document.querySelectorAll(
+  "#questions-container > div"
+);
+allDivsContainingQuestionsNode.forEach(function (element) {
+  element.classList.add("display-not");
+});
+allDivsContainingQuestionsNode[0].classList.remove("display-not");
+let allButtonsWithAnswersNodes = document.getElementsByClassName("btn");
+for (let node of allButtonsWithAnswersNodes) {
+  node.addEventListener("click", function () {
+    node.classList.toggle("selected");
+  });
 }
 
+let submitBtn = document.querySelector("#submitBtn");
 
+submitBtn.addEventListener(
+  "click",
+  (queTrans = function () {
+    let allQuestionsDivsNode = document.querySelectorAll(
+      "#questions-container >div"
+    );
+    let questionNumberSpan = document.getElementById("questionNumber");
+    questionNumberSpan.innerText = `${2}`;
 
-let submitBtn= document.querySelector('#submitBtn')
+    questionCount += 1;
+    for (let index = 0; index < questions.length; index++) {
+      if (!allQuestionsDivsNode[index].classList.contains("display-not")) {
+        allQuestionsDivsNode[index].classList.add("display-not");
 
+        allQuestionsDivsNode[index + 1].classList.remove("display-not");
 
-submitBtn.addEventListener('click',queTrans =function (){
-  
-  
-let allQuestionsDivsNode = document.querySelectorAll('#questions-container >div')
-let questionNumberSpan = document.getElementById("questionNumber")
-questionNumberSpan.innerText =`${2}`
- 
-  questionCount +=1
-  for (let index = 0; index < questions.length; index++) {
-    
-    if (!allQuestionsDivsNode[index].classList.contains('display-not')  ) {
-      
-      allQuestionsDivsNode[index].classList.add('display-not')
-      
-      allQuestionsDivsNode[index+1].classList.remove('display-not')
-      
-      return 1
-
-      
-    };
-    if(questionCount === 11){
-      submitBtn.classList.add('display-not')
-      let proceedBtnNode = document.getElementById('proceedBtn')
-      proceedBtnNode.classList.remove('display-not')
-      questionNumberSpan.classList.add('display-none')
-      // let aTagNode =document.createElement('a')
-      // aTagNode.setAttribute('href','./result.html')
-      // submitBtn.appendChild(aTagNode)
-
-
+        return 1;
+      }
+      if (questionCount === 11) {
+        submitBtn.classList.add("display-not");
+        let proceedBtnNode = document.getElementById("proceedBtn");
+        proceedBtnNode.classList.remove("display-not");
+        questionNumberSpan.classList.add("display-none");
+        // let aTagNode =document.createElement('a')
+        // aTagNode.setAttribute('href','./result.html')
+        // submitBtn.appendChild(aTagNode)
+      }
+      questionNumberSpan.innerText = `${questionCount}`;
     }
-    questionNumberSpan.innerText =`${questionCount}`
-  }
-  
-  return questionCount
-})
 
+    return questionCount;
+  })
+);
 
-
-
-
-
-
-allQuestions= document.querySelectorAll('#questions-container > div')
-checkIfAnswerSelectedIsCorrect = function(){
-  
- 
-  let long =0
-  let currentButton
+allQuestions = document.querySelectorAll("#questions-container > div");
+checkIfAnswerSelectedIsCorrect = function () {
+  let long = 0;
+  let currentButton;
   for (let index = 0; index < questions.length; index++) {
-    long = allQuestions[index].querySelectorAll('button').length
-    
+    long = allQuestions[index].querySelectorAll("button").length;
+
     for (let j = 0; j < long; j++) {
-      currentButton = allQuestions[index].querySelectorAll('button')[j]
-       if(currentButton.classList.contains('selected')&& (currentButton.id==('correct'))){
-         correctAnswersCount++;
-
-
-       }
-      
-      
+      currentButton = allQuestions[index].querySelectorAll("button")[j];
+      if (
+        currentButton.classList.contains("selected") &&
+        currentButton.id == "correct"
+      ) {
+        correctAnswersCount++;
+      }
     }
-   
-}
+  }
 
-return correctAnswersCount
-}
-
-
-
-  
- 
-
+  return correctAnswersCount;
+};
 
 // makeSureOnlyOneAnswerGetsSelected = function(){
 //   for (let index = 0; index < questions.length; index++) {
@@ -230,7 +207,7 @@ return correctAnswersCount
 //             };
 //             break;
 //            case 1:
-            
+
 //               allQuestions[index].querySelectorAll('button')[0].classList.remove('selected');
 //               allQuestions[index].querySelectorAll('button')[2].classList.remove('selected');
 //               allQuestions[index].querySelectorAll('button')[3].classList.remove('selected');
@@ -244,185 +221,140 @@ return correctAnswersCount
 //                 allQuestions[index].querySelectorAll('button')[0].classList.remove('selected');
 //               allQuestions[index].querySelectorAll('button')[1].classList.remove('selected');
 //               allQuestions[index].querySelectorAll('button')[2].classList.remove('selected');
-              
-
-
-
-            
 
 //          }
-         
 
 //         }
-      
+
 //       }
-   
+
 //     }
 
 //   }
-//  
+//
 
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
 
+const COLOR_CODES = {
+  info: {
+    color: "green",
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD,
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD,
+  },
+};
 
+const TIME_LIMIT = 10;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+submitBtn.addEventListener("click", function () {
+  console.log("first");
+  timeLeft = 10;
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- const FULL_DASH_ARRAY = 283;
- const WARNING_THRESHOLD = 10;
- const ALERT_THRESHOLD = 5;
-
- const COLOR_CODES = {
-   info: {
-     color: "green",
-   },
-   warning: {
-     color: "orange",
-     threshold: WARNING_THRESHOLD,
-   },
-   alert: {
-     color: "red",
-     threshold: ALERT_THRESHOLD,
-   },
- };
-
-
-
- const TIME_LIMIT = 10;
- let timePassed = 0;
- let timeLeft = TIME_LIMIT;
- let timerInterval = null;
- let remainingPathColor = COLOR_CODES.info.color;
-
- document.getElementById("app").innerHTML = `
+document.getElementById("app").innerHTML = `
 <div class="base-timer">
-<svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-<g class="base-timer__circle">
- <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
- <path
-   id="base-timer-path-remaining"
-   stroke-dasharray="283"
-   class="base-timer__path-remaining ${remainingPathColor}"
-   d="
-     M 50, 50
-     m -45, 0
-     a 45,45 0 1,0 90,0
-     a 45,45 0 1,0 -90,0
-   "
- ></path>
-</g>
-</svg>
-<span id="base-timer-label" class="base-timer__label">${formatTime(
-timeLeft
-)}</span>
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
 </div>
 `;
 
+startTimer();
 
-let myTime = new Date().getSeconds();
-let mySec = Number(myTime.toString().slice(0,2));
- startTimer();
+function onTimesUp() {
+  setInterval(timerInterval);
+}
 
- function onTimesUp() {
-   clearInterval(timerInterval);
- }
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML =
+      formatTime(timeLeft);
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
 
- function startTimer() {
-    setInterval(() => {
-     timePassed = timePassed += 1;
-     timeLeft = TIME_LIMIT - timePassed;
-     document.getElementById("base-timer-label").innerHTML =
-       formatTime(timeLeft);
-     setCircleDasharray();
-     setRemainingPathColor(timeLeft);
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
 
-     if (timeLeft === 0) {
-        console.log("Times Up");
-    //    onTimesUp();
-        timeLeft = 10;
-      //  randomQuestion();
+function formatTime(time) {
+  // const minutes = Math.floor(time / 60);
+  // let seconds = time % 60;
+  let seconds = time;
 
-     }
-   }, 1000);
- }
- 
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
 
- function formatTime(time) {
-   // const minutes = Math.floor(time / 60);
-   // let seconds = time % 60;
-   let seconds = time;
+  // return `${minutes}:${seconds}`;
+  return `${seconds}`;
+}
 
-   if (seconds < 10) {
-     seconds = `0${seconds}`;
-   }
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
 
-   // return `${minutes}:${seconds}`;
-   return `${seconds}`;
- }
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
 
- function setRemainingPathColor(timeLeft) {
-   const { alert, warning, info } = COLOR_CODES;
-   if (timeLeft <= alert.threshold) {
-     document
-       .getElementById("base-timer-path-remaining")
-       .classList.remove(warning.color);
-     document
-       .getElementById("base-timer-path-remaining")
-       .classList.add(alert.color);
-   } else if (timeLeft <= warning.threshold) {
-     document
-       .getElementById("base-timer-path-remaining")
-       .classList.remove(info.color);
-     document
-       .getElementById("base-timer-path-remaining")
-       .classList.add(warning.color);
-   }
- }
-
- function calculateTimeFraction() {
-   const rawTimeFraction = timeLeft / TIME_LIMIT;
-   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
- }
-
- function setCircleDasharray() {
-   const circleDasharray = `${(
-     calculateTimeFraction() * FULL_DASH_ARRAY
-   ).toFixed(0)} 283`;
-   document
-     .getElementById("base-timer-path-remaining")
-     .setAttribute("stroke-dasharray", circleDasharray);
- }
-
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
 // STARTING OF THE QUESTIONS ARRAY
 
 // SELECTING THE ELEMENTS
 
-
-
 // console.log(`this is my question ${question}`);
 // console.log(`Correct answer ${correctAnswer}`);
 // console.log(`incorrect answer ${incorrectAnswers}`);
-
-
-
-
-
-
-
-
--- javascript-file/result.js
+++ javascript-file/result.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  setProgress(90);
+  setProgress(percentage);
 };
 let progressCircle = document.querySelector(".progress");
 let radius = progressCircle.r.baseVal.value;
@@ -9,10 +9,27 @@ progressCircle.style.strokeDasharray = circumference;
 
 //0 to 100
 // setProgress(80);
+let rightPercentage = document.getElementById("correctPresentage");
+let wrongPercentage = document.getElementById("wrongPresentage");
+let rightCount = document.getElementById("rightCount");
+let wrongCount = document.getElementById("wrongCount");
+
+let questionNum = 9;
+// let correctNum = document.getElementById("calPresentage").value;
+let correctNum = 8;
+console.log(correctNum);
+
+let percentage = (questionNum / 100) * (correctNum * 100);
+console.log(percentage);
 
 function setProgress(percent) {
   progressCircle.style.strokeDashoffset =
     circumference - (percent / -100) * circumference;
   progressCircle.classList.add("circlAnimation");
-  progressCircle.setAttribute("stroke-dashoffset", persent * 10);
+  progressCircle.setAttribute("stroke-dashoffset", percent * 10);
+  rightPercentage.innerText = percentage + " %";
+  wrongPercentage.innerText = 100 - percentage + " %";
+  rightCount.innerText = correctNum + "/" + questionNum + " questions";
+  wrongCount.innerText =
+    questionNum - correctNum + "/" + questionNum + " questions";
 }
